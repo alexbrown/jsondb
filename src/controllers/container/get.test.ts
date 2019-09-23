bae3356aecef705a26ad8d61b32f4a421bdac218
Alex Brown <alexrobrown@gmail.com>
2019-09-23 16:46:44 -0400

    Adding jest and some basic tests

---
+++ src/controllers/container/get.test.ts
@@ -0,0 +1,10 @@
+import get from './get';
+import { Request } from 'express';
+import Response from '../../test/helpers/response';
+
+test('controller returns correct string', () => {
+    const req = {} as Request;
+    const res = new Response() as any;
+    get(req, res);
+    expect(res.message).toEqual('This is the endpoint to get a specific object in the container or a collection');
+});
---
+++ src/controllers/container/save.test.ts
@@ -0,0 +1,10 @@
+import save from './save';
+import { Request } from 'express';
+import Response from '../../test/helpers/response';
+
+test('controller returns correct string', () => {
+    const req = {} as Request;
+    const res = new Response() as any;
+    save(req, res);
+    expect(res.message).toEqual('This is the endpoint to save/update an object in the container or a collection');
+});
