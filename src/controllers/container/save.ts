a3d0fd7b5371adf2cb9a9548e6c9cea80c34fcdd
Alex Brown <alexrobrown@gmail.com>
2019-09-23 20:59:51 -0400

    saving and updating records in db

--- src/controllers/container/save.ts
+++ src/controllers/container/save.ts
@@ -1,5 +1,31 @@
 import { Request, Response } from "express";
+import {getParams} from '../../helpers/request';
+import db from '../../models/entry';
+const Entry = db.getInstance();
 
-export default (req: Request, res: Response) => {
-    res.send("This is the endpoint to save/update an object in the container or a collection");
+export default async (req: Request, res: Response) => {
+    const now = new Date();
+    const params = getParams(req.params);
+    const { body } = req;
+    const entry = buildEntry(params, body);
+    
+    const existingDocument = await Entry.findOne({_container: params._container, _collection: params._collection});
+    let record;
+    if (existingDocument) {
+        entry['_id'] = existingDocument._id; 
+        entry['updatedAt'] = now;
+        record = await entry.updateOne(entry);
+    } else {
+        entry['createdAt'] = now;
+        record = await entry.save();
+    }
+    res.send(record);
+}
+
+function buildEntry(params: any, body: any) {
+    return new Entry({
+        _container: params._container,
+        _collection: params._collection,
+        content: JSON.stringify(body)
+    })
 }
